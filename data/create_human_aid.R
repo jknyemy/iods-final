db173e7fc792804885c5dc73ee7deb7ca7a12424
jknyemy <janne.kirjasniemi@gmail.com>
2017-03-08 17:47:45 +0200

    More wrangling

--- data/create_human_aid.R
+++ data/create_human_aid.R
@@ -5,7 +5,7 @@
 #And Gapminder:
 #https://www.gapminder.org/data/;
 #For foreign aid data provided by World Bank
-#For Child deaths data provided by Lance
+#For Child deaths and women's fertility data provided by Gapminder
 
 library(dplyr)
 library(xlsx)
@@ -40,8 +40,87 @@ hdi3$Country <- as.character(hdi3$Country)
 #Labour participation and >1 means there are more women than men
 #and 1 naturally is a 50-50 split.
 
-HDI.FM <- transmute(hdi2, HDI.FM = HDI.F / HDI.M)
-Life.expFM <- transmute(hdi2, Life.expFM = Life.exp.F / Life.exp.M)
-str(hdi2)
-Exp.SchoolFM <- transmute(hdi2, Exp.SchoolFM = Exp.school.F/Exp.school.M)
-Mean.SchoolFM <- transmute(hdi2, Mean.SchoolFM = Mean.School.F/MeanSchool.M)
+#This could be done more elegantly, but this gets the job done as well.
+hdi2_1 <- transmute(hdi2, HDI.FM = HDI.F / HDI.M) 
+hdi2_1 <- bind_cols(hdi2_1, transmute(hdi2, Life.expFM = Life.exp.F / Life.exp.M))
+hdi2_1 <- bind_cols(hdi2_1, transmute(hdi2, Exp.SchoolFM = Exp.school.F/Exp.school.M))
+hdi2_1 <- bind_cols(hdi2_1, transmute(hdi2, Mean.SchoolFM = Mean.School.F./Mean.School.M))
+hdi2_1 <- bind_cols(hdi2_1, transmute(hdi2, Est.GNIFM = Est.GNI.F / Est.GNI.M))
+head(hdi2_1)
+hdi3_1 <- transmute(hdi3, Edu2.FM = Edu2.F/Edu2.M)
+hdi3_1 <- bind_cols(hdi3_1, transmute(hdi3, Labour.FM = Labour.F/Labour.M))
+str(hdi1)
+hdi.aid <- bind_cols(hdi2_1, hdi3_1)
+names(hdi1)[names(hdi1) == "HDI."] <- "HDI"
+hdi.aid <- select(hdi.aid, -Country)
+
+hdi.aid <- bind_cols(hdi1, hdi.aid)
+str(hdi.aid)
+#Sorting alphabetically by country name so it is easier to combine
+#with other data.
+hdi.aid2 <- hdi.aid[order(hdi.aid$Country),]
+
+#Loading the Gapminder women's fertility data, selecting the year 20
+#2014 and deleting missing values.
+fert <- read.xlsx2("indicator undata total_fertility_1.xlsx", sheetIndex=1)
+str(fert)
+keep_columns <- c("Country", "X2014")
+fert2014 <- select(fert, one_of(keep_columns))
+fert2014$X2014 <- as.numeric(levels(fert2014$X2014))[fert2014$X2014]
+fert2014$Country <- as.character(fert2014$Country)
+fert2014$miss <- complete.cases(fert2014)
+fert2014_1 <- filter(fert2014, miss==TRUE)
+fert2014_1 <- select(fert2014_1, -miss)
+names(hdi.aid3)[names(hdi.aid3) == "X2014"] <- "Fert"
+
+#Combining the data
+hdi.aid3 <- inner_join(hdi.aid2, fert2014_1, by = "Country")
+View(hdi.aid3)
+
+#Loading Gapminder's data on foreign aid received provided by World Bank
+#The point of this data is to get an indication of aid received which 
+#which would have had time to affect the country in question. 
+#To this end I will calculate a mean of aid received a 20 year
+#period to year 2004, which is 10 years before
+#the other data. Also a column that tells the amount of years that mean is
+#calculated from, to give an indication of how consistent the aid
+#has been.
+
+aid <- read.xlsx2("indicator_aid received % of gni_1.xlsx", sheetIndex=1)
+aid2 <- select(aid, X1984:X2004)
+
+indx <- sapply(aid2, is.factor)
+aid2[indx] <- lapply(aid2[indx], function(x) as.numeric(as.character(x)))
+
+str(aid2)
+aid$mean04 <- rowMeans(aid2, na.rm=TRUE)
+mean.length <- apply(aid2, MARGIN = 1, FUN = function(x) length(x[!is.na(x)]) )
+aid$mean.length <- mean.length
+keep_columns2 <- c("Country", "mean04", "mean.length")
+aid3 <- select(aid, one_of(keep_columns2))
+View(aid3)
+
+#Combining data.
+
+hdi.aid4 <- inner_join(hdi.aid3, aid3, by = "Country")
+View(hdi.aid4)
+
+#Loading under 5 child mortality/1000 live births for the year
+#2014 from Gapminder and combining it o the dataset.
+
+mort <- read.xlsx2("indicator gapminder under5mortality_1.xlsx", sheetIndex = 1)
+keep_columns3 <- c("Country", "X2014")
+child.mort <- select(mort, one_of(keep_columns3))
+names(child.mort)[names(child.mort) == "X2014"] <- "child.mort"
+View(child.mort)
+child.mort$Country <- as.character(child.mort$Country)
+child.mort$child.mort <- as.numeric(child.mort$child.mort)
+str(child.mort)
+
+#Combining data
+
+hdi.aid5 <- inner_join(hdi.aid4, child.mort, by = "Country")
+View(hdi.aid5)
+#Saving the dataset
+
+write.csv2(hdi.aid5, "hdi_aid.csv")
